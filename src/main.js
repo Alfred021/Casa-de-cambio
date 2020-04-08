108fe99fbf9f954d6baee9a8c8011a3532762efc
Alfred021 <alfredomedina10@hotmail.com>
2020-04-08 13:59:42 -0300

    Cambiando la lista existente por un dropdown
    Ajustado el main.js para adaptarse al dropdown

--- src/main.js
+++ src/main.js
@@ -20,21 +20,21 @@ function configureInputDate() {
     var year = todaysDate.getFullYear();                        
     var month = ("0" + (todaysDate.getMonth() + 1)).slice(-2);  
     var day = ("0" + todaysDate.getDate()).slice(-2);           
-    var maxDate = (year +"-"+ month +"-"+ day); // Results in "YYYY-MM-DD" for today's date 
+    var maxDate = (year +"-"+ month +"-"+ day);
     $date.setAttribute('max', maxDate);
     $date.addEventListener('change', updateData)
 }
 
 function listOfCurrencies(currency) {
-    $list = document.querySelector('.list-group')
+    $list = document.querySelector('.dropdown-menu')
     currency.forEach((baseCurrency) => {
         const $item = document.createElement('a');
         $item.href = '#';
-        $item.classList.add('list-group-item', 'list-group-item-action');
+        $item.classList.add('dropdown-item')
         $item.textContent = baseCurrency;
         $item.dataset.baseCurrency = baseCurrency;
         $item.addEventListener('click', () => {
-            const $activeElement = document.querySelector('.list-group-item.active');
+            const $activeElement = document.querySelector('.dropdown-item.active');
             if ($activeElement) {
                 $activeElement.classList.remove('active');
             }
@@ -46,9 +46,9 @@ function listOfCurrencies(currency) {
 }
 
 function getSelectedCurrency() {
-    const $activeCurrency = document.querySelector('.list-group-item.active');
+    const $activeCurrency = document.querySelector('.dropdown-item.active');
     if ($activeCurrency) {
-        return document.querySelector('.list-group-item.active').dataset.baseCurrency;
+        return document.querySelector('.dropdown-item.active').dataset.baseCurrency;
     }
     return undefined;
 }
