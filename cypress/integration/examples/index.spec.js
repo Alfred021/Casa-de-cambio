c8017c7dae8c3ec34cf69e2eadf6cf63fe073e11
Alfred021 <alfredomedina10@hotmail.com>
2020-10-13 12:31:50 -0300

    Completados los test en cypresst
    Test funcionales de todas las partes de la aplicacion incluyendo el cambio de valor de la moneda en distintas fechas, utlizando fixtures para los network requests

--- cypress/integration/examples/index.spec.js
+++ cypress/integration/examples/index.spec.js
@@ -1,16 +1,61 @@
-/* eslint-disable linebreak-style */
-describe('Open Website', () => {
-  it('successfully loads', () => {
-    cy.visit('/');
+/// <reference types="Cypress" />
+
+describe('casa de cambio', () => {
+  const URL = '/';
+  const polyfillUrl = 'https://unpkg.com/unfetch/dist/unfetch.umd.js';
+  let polyfill;
+
+  // grab fetch polyfill from remote URL, could be also from a local package
+  before(() => {
+    cy.request(polyfillUrl)
+      .then((response) => {
+        polyfill = response.body;
+      });
+
+    cy.visit(URL, {
+      onBeforeLoad(contentWindow) {
+        delete contentWindow.fetch;
+        contentWindow.eval(polyfill);
+        contentWindow.fetch = contentWindow.unfetch;
+      },
+    });
+  });
+
+  beforeEach(() =>{
+    cy.server();
+    cy.route('GET', 'https://api.exchangeratesapi.io', 'fixture: exchange.json')
   });
 
   it('shows currencies', () => {
-    const $dropdownMenu = cy.get('#dropdownMenu2')
+    cy.get('#dropdownMenu2')
       .click()
-      .then(($dropdownMenu) => {
+      .then(() => {
         cy.get('.dropdown-menu.dropdown-menu-lg-right')
           .find('.dropdown-item').should('have.length', 33);
       });
   });
+
+  it('show currencies and their spots', () => {
+    const $listItem = cy.get('.dropdown-item').contains('CAD')
+      .click().should('have.class', 'active')
+      .then(($listItem), () => {
+        cy.get('tbody')
+          .find('tr').should('have.length', 33);
+        cy.get('tr')
+          .find('td').should('have.length', 66);
+      });
+  });
   
+  it('set date and verify changing spots', () => {
+    cy.get('td').contains('USD').siblings().invoke('text').then((text1) => {
+      cy.get('input[type="date"]')
+        .type('2020-09-15')
+        .trigger('change')
+      cy.get('#Rates tbody')
+        .find('tr');
+      cy.get('td').contains('USD').siblings().invoke('text').should((text2) => {
+        expect(text1).not.to.eq(text2)
+      })
+    });
+  })
 });
